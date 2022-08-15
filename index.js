3846bed0d8309645d77111835ae5d3cb4a58c807
thanmaymdas <69765216+tAnboyy@users.noreply.github.com>
2022-08-15 19:21:18 +0530

    Update index.js
    typo correction

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello" }');
 });
 
 app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": "It works!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
